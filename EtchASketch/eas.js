111986afa23f42fa21d71e7e2f4af9a65a5e3c01
Jacob <jluallen@fcc-jc.org>
2021-04-01 15:25:51 -0400

    added for loop to make div rows.  Doesn't seem to be working though.

--- EtchASketch/eas.js
+++ EtchASketch/eas.js
@@ -3,17 +3,25 @@ btn.addEventListener('click', makeGrid);
 
 function makeGrid () {
     let widthAndHeight = 960/16;
-    for (let i = 0; i < 16; i++) {
+    for(let j = 0; j < 16; j ++) {
         const container = document.querySelector("#container");
-        const etchPiece = document.createElement("div");
-        etchPiece.setAttribute('class', "etchBlock");
-        etchPiece.style.width = widthAndHeight + "px";
-        etchPiece.style.height =  "960px"; 
-        etchPiece.style.backgroundColor = "grey";
-        etchPiece.style.borderColor = "pink";
-        etchPiece.style.borderWidth = "10px";
-        etchPiece.addEventListener('hover', setBlack(etchPiece));
-        container.appendChild(etchPiece);
+        const eachRow = document.createElement("div");
+        eachRow.setAttribute('id', "eachRow" + j);
+        eachRow.style.width = "100%";
+        eachRow.style.height = widthAndHeight + "px";
+        eachRow.style.backgroundColor = "grey";
+        /*for (let i = 0; i < 16; i++) {
+            const fillRow = document.querySelector(".eachRow" + j);
+            const etchPiece = document.createElement("div");
+            etchPiece.setAttribute('class', "etchBlock");
+            etchPiece.style.width = widthAndHeight + "px";
+            etchPiece.style.height =  widthAndHeight + "px"; 
+            etchPiece.style.backgroundColor = "grey";
+            etchPiece.style.borderColor = "pink";
+            etchPiece.style.borderWidth = "10px";
+            etchPiece.addEventListener('hover', setBlack(etchPiece));
+            fillRow.appendChild(etchPiece);
+        }*/
     }
 }
 function setBlack(etch) {
